8387602fb59b08a4fe8b17956acf5c48ce7861e6
soffph <soff.dg18@gmail.com>
2018-06-29 20:53:34 -0500

    capitulo 15 camera

---
+++ Capitulo 15/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by sofia delgado gonzalez on 6/29/18.
+//  Copyright © 2018 Soff. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+}
---
+++ Capitulo 15/Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by sofia delgado gonzalez on 6/29/18.
+//  Copyright © 2018 Soff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
